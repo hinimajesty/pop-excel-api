232dee788a302286e8ce1ddd41becc4c94639e05
= <=>
2017-10-16 22:05:34 -0700

    inserted the dataByColumn method here

--- src/Helpers.php
+++ src/Helpers.php
@@ -53,4 +53,30 @@ class Helpers {
        return $_FILES[ $form_input_field ][ "name" ];
      }
 
+     /**
+      * get the data in the excel file 
+      *
+      * @param  int $colum_index
+      * @return array $dataInColumn
+      * @return string err1
+      * @return string err2
+      */
+      
+      public function getDataByColumn($column_index){
+          if( ! $this->isEmpty( ) ){
+            $this->file_data = fopen($_FILES[$this->form_input_field]["tmp_name"], "r");
+             if( $this->isValidExt() ){
+               while ( $column = fgetcsv($this->file_data) ) {
+                   if( ! empty($column[$column_index]) ){
+                   array_push( $this->dataInColumn, trim( $column[$column_index] ) );
+                   }
+               } 
+               return $this->dataInColumn; 
+             }
+               return "err1";
+          }
+         return "err2"; 
+      }
+
+
 } #end of Helpers class 
